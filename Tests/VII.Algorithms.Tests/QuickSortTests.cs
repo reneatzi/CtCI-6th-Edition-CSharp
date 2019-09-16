535e85f0e0ccf6b86db4d7808af750b4307b52e5
René Atzenhofer <reneatzi@users.noreply.github.com>
2019-09-16 13:27:40 +0200

    Quicksort: Add basic tests

---
+++ Tests/VII.Algorithms.Tests/QuickSortTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VII.Algorithms.Tests
+{
+	[TestClass]
+	public class QuickSortTests
+	{
+		[TestMethod]
+		public void TestSimpleArray_Should_ReturnSortedArray()
+		{
+			var arrayToSort = new[] {5, 3, 1, 4, 7, -7, 50, 2000};
+
+			new QuickSort().Sort(arrayToSort);
+
+			arrayToSort.Should().BeInAscendingOrder();
+			arrayToSort.Length.Should().Be(8);
+		}
+
+		[TestMethod]
+		public void TestArrayWithDuplicates_Should_ReturnSortedArray()
+		{
+			var arrayToSort = new[] {5, 3, 3, 3, 7, -7, 50, 2000};
+
+			new QuickSort().Sort(arrayToSort);
+
+			arrayToSort.Should().BeInAscendingOrder();
+			arrayToSort.Length.Should().Be(8);
+		}
+
+		[TestMethod]
+		public void TestNegativeOnly_Should_ReturnSortedArray()
+		{
+			var arrayToSort = new[] {-5, -3, -3, -3, -19234123, -7, -50, -2000};
+
+			new QuickSort().Sort(arrayToSort);
+
+			arrayToSort.Should().BeInAscendingOrder();
+			arrayToSort.Length.Should().Be(8);
+		}
+	}
+}
