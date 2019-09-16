eb1b59381126e72c84268bddd3efc2bb0ae75968
René Atzenhofer <reneatzi@users.noreply.github.com>
2019-09-16 07:40:48 +0200

    Quicksort: Add unoptimized algorithm

--- VII.Algorithms/Program.cs
+++ VII.Algorithms/Program.cs
@@ -6,7 +6,23 @@ namespace VII.Algorithms
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var arrayToSort = new int[] {2, 5, -4, 11, 0, 18, 22, 67, 51, 6};
+
+			PrintArray(arrayToSort);
+
+			new QuickSort().Sort(arrayToSort);
+
+			PrintArray(arrayToSort);
+		}
+
+		private static void PrintArray(int[] arrayToSort)
+		{
+			Console.WriteLine("Array: ");
+
+			foreach (var item in arrayToSort)
+				Console.Write(" " + item);
+
+			Console.WriteLine();
 		}
 	}
 }
